ff6fec3fea88df1e7dc71e1ccfd557b0ccb397a9
RAnderson96 <rory.cameron.anderson@gmail.com>
2023-03-01 11:32:09 +0000

    Created a Celsius to Fahrenheit converter app

---
+++ src/main/java/com/mycompany/celsiusconverter/CelsiusConverter.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.celsiusconverter;
+
+/**
+ *
+ * @author admin
+ */
+public class CelsiusConverter {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
